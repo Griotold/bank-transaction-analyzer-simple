afde29bd9c65ae284a732208bdd5b119e2a82b2e
Griotold <gotjd9773@naver.com>
2024-03-09 11:06:35 +0900

    Initial commit

---
+++ src/main/java/com/griotold/banktransactionanalyzersimple/BankTransactionAnalyzerSimpleApplication.java
@@ -0,0 +1,13 @@
+package com.griotold.banktransactionanalyzersimple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankTransactionAnalyzerSimpleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankTransactionAnalyzerSimpleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/griotold/banktransactionanalyzersimple/BankTransactionAnalyzerSimpleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.griotold.banktransactionanalyzersimple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankTransactionAnalyzerSimpleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
