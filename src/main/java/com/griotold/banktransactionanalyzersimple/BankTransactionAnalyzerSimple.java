c5fd9e5405682cb8e27f62bc645cc5b10730fa88
Griotold <gotjd9773@naver.com>
2024-03-09 15:08:23 +0900

    함수형 인터페이스를 구현하는 클래스를 만드는 건 번거롭다. 자바 8의 람다표현식을 사용하면 편리하다.

--- src/main/java/com/griotold/banktransactionanalyzersimple/BankTransactionAnalyzerSimple.java
+++ src/main/java/com/griotold/banktransactionanalyzersimple/BankTransactionAnalyzerSimple.java
@@ -27,9 +27,15 @@ public class BankTransactionAnalyzerSimple {
         System.out.println("The total salary received is " + bankStatementProcessor.calculateTotalForCategory("Salary"));
 
         // OCP 적용
-        System.out.println("The total for transactions in February And Expesnsive is "
+        System.out.println("The total for transactions in February And Expensive is "
                 + bankStatementProcessor.findTransaction(new BankTransactionIsInFebruaryAndExpensive() {
         }));
+
+        // 람다 표현식을 적용하면 함수형 인터페이스를 구현할 필요가 없다.
+        System.out.println("The toal for transactions in February And Expensive is "
+                + bankStatementProcessor.findTransaction((bankTransaction) ->
+                bankTransaction.getDate().getMonth() == Month.FEBRUARY
+                        && bankTransaction.getAmount() >= 1_000));
     }
 
 }
