2201d3cc6cd00e6c11b2de998125098452b44f70
Griotold <gotjd9773@naver.com>
2024-03-09 11:30:25 +0900

    1월 입출금 내역 합계 계산하기

--- src/main/java/com/griotold/banktransactionanalyzersimple/BankTransactionAnalyzerSimple.java
+++ src/main/java/com/griotold/banktransactionanalyzersimple/BankTransactionAnalyzerSimple.java
@@ -4,6 +4,9 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 public class BankTransactionAnalyzerSimple {
@@ -14,13 +17,19 @@ public class BankTransactionAnalyzerSimple {
         final Path path = Paths.get(RESOURCES + args[0]);
         final List<String> lines = Files.readAllLines(path);
         double total = 0d;
+        final DateTimeFormatter DATE_PATTERN = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+
+        // 1월 입출금 내역 합계
         for (String line : lines) {
             final String[] columns = line.split(",");
-            final double amount = Double.parseDouble(columns[1]);
-            total += amount;
+            LocalDate date = LocalDate.parse(columns[0], DATE_PATTERN);
+            if (date.getMonth() == Month.JANUARY) {
+                final double amount = Double.parseDouble(columns[1]);
+                total += amount;
+            }
         }
 
-        System.out.println("The total for all transactions is " + total);
+        System.out.println("The total for all transactions in January is " + total);
     }
 
 }
